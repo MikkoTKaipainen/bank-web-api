b729ddea1e2ed4a96958bcc199330771f0c784cc
Timo Leppänen <timo.leppanen85@gmail.com>
2019-01-31 09:47:12 +0200

    Groundlevel

---
+++ BankWepApi/Services/ITransactionService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankWepApi.Services
+{
+    public class ITransactionService
+    {
+        // CRUD
+        
+    }
+}
