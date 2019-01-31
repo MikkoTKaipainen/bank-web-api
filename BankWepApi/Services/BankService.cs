7afda27cb386c800f5a00f3afcb269517dae0dbc
Mikko Kaipainen <mikko.kaipainen@student.saimia.fi>
2019-01-31 11:09:46 +0200

    Startup services and injections

---
+++ BankWepApi/Services/BankService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BankWepApi.Models;
+
+namespace BankWepApi.Services
+{
+    public class BankService : IBankService
+    {
+        public Bank CreateBank(Bank bank)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Bank DeleteBank(Bank bank, int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Bank> ReadBank(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Bank ReadBank(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Bank> ReadBanks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Bank UpdateBank(Bank bank, int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
