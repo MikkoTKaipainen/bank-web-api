43da98511e444005867fd130cda7d2f33c094595
Iiro Kutvonen <kutvonen.iiro@gmail.com>
2019-02-14 22:54:18 +0200

    BANK
    bankendorfen, hjälpa mig, den är schaiße

---
+++ BankWepApi/Controllers/BanksController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BankWepApi.Models;
+using BankWepApi.Repositories;
+
+namespace BankWepApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BanksController : ControllerBase
+    {
+        private readonly IBankRepository _bankRepository;
+
+        public BanksController(IBankRepository bankRepository)
+        {
+            _bankRepository = bankRepository;
+        }
+
+      
+        // GET: api/Banks
+        [HttpGet]
+        public ActionResult<List<Bank>> GetBanks()
+        {
+            return new JsonResult(_bankRepository.Read());
+        }
+
+        // GET: api/Banks/5
+        [HttpGet("{id}")]
+        public ActionResult<Bank> Get(int id)
+        {
+            var bank = _bankRepository.Read(id);
+            return new JsonResult();
+        }
+
+        // PUT: api/Banks/5
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // POST: api/Banks
+        [HttpPost]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/Banks/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+              
+    }
+}
---
+++ BankWepApi/Repositories/BankRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BankWepApi.Models;
+
+namespace BankWepApi.Repositories
+{
+    public class BankRepository : IBankRepository
+    {
+
+        private readonly BackendbankdbContext _context;
+
+        public BankRepository(BackendbankdbContext context)
+        {
+            _context = context;
+        }
+        public Bank CreateBank(Bank bank)
+        {
+            _context.Add(bank);
+            _context.SaveChanges();
+            return bank;
+        }
+
+        public Bank DeleteBank(Bank bank)
+        {
+            var deletedBank = ReadBank(id);
+            _context.Bank.Remove(deletedBank);
+            _context.SaveChanges();
+            return bank;
+        }
+
+        public Bank ReadBank(int id)
+        {
+            return _context.Bank
+                .Where(b => b.Id == id)
+                .FirstOrDefault(b => b.Id == id);
+        }
+
+        public List<Bank> ReadBank(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Bank> ReadBanks()
+        {
+            return _context.Bank
+                .ToList();
+        }
+
+        public Bank UpdateBank(Bank bank)
+        {
+            _context.Update(bank);
+            _context.SaveChanges();
+            return bank;
+        }
+    }
+}
